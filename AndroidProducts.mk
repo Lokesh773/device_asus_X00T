4bb00beb3155d99c8a1fa8c2e70866ee8666161a
lokesh <43849744+Lokesh773@users.noreply.github.com>
2020-05-22 14:04:17 +0530

    X00T:Adapt to zenx

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X00T.mk
+    $(LOCAL_DIR)/zenx_X00T.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X00T-user \
-    lineage_X00T-userdebug \
-    lineage_X00T-eng
+    zenx_X00T-user \
+    zenx_X00T-userdebug \
+    zenx_X00T-eng
