0448c4b9a764c950f4351fbf79132b255086d170
alexmercer09 <supremegod.of.war@gmail.com>
2020-05-25 12:05:23 +0000

    Initial changes for Pixys
    Signed-off-by: alexmercer09 <supremegod.of.war@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X00T.mk
+    $(LOCAL_DIR)/pixys_X00T.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X00T-user \
-    lineage_X00T-userdebug \
-    lineage_X00T-eng
+    pixys_X00T-userdebug \
+
