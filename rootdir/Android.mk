1d0c018b62c9cbce1814d3d476fd1ae61c2c3ff6
Lokesh773 <lokeshkasamneni773@gmail.com>
2020-08-27 09:19:42 +0200

    X00T:Add spectrum support
    Signed-off-by: Lokesh773 <lokeshkasamneni773@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -27,6 +27,23 @@ LOCAL_SRC_FILES    := bin/init.qcom.post_boot.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
+# Spectrum scripts
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.spectrum.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.spectrum.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.spectrum.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.spectrum.sh
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.sensors.sh
 LOCAL_MODULE_TAGS  := optional
